c890d8a3f64e19dba55ba776557b888834c7e417
Cybe Plax <Cybe@Plax.de>
2021-06-06 23:46:58 +0200

    hotfix target data csv name

--- merge_data.py
+++ merge_data.py
@@ -18,4 +18,4 @@ page_views = load('pageviews_st.peter.ording', header=None)
 page_views['day'] = page_views['date']
 weather_spo = weather_spo.merge(page_views, on='day', how='left')
 weather_spo = weather_spo[['date_x', 'wind_speed', 'wind_direction', 'wind_gust', 'temp', 'count']]
-weather_spo.to_csv('data/_weather_data_10044N_2016-2019.csv', index=False)
+weather_spo.to_csv('data/_weather_data_10028_2016-2019.csv', index=False)
